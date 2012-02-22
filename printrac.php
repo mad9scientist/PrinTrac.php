d3165de00b9ff136be66bdc1928cb5c548fad651
Chris Holbrook <chris@madscitech.com>
2012-02-22 05:20:10 -0600

    Added Boilerplate comments

--- printrac.php
+++ printrac.php
@@ -1,8 +1,68 @@
 <?php 
 
 /*
-	PrinTrac.php
+	PrinTrac.php (v. 0.0.1)
 		Get Stats on Users Printing Your Web Pages
 		Created by: Chris Holbrook (http://mad9scientist.com/)
-		GitHub: 
+		GitHub: http://mad9scientist.github.com/printrac.php
+*/
+
+/* Google Analytics */
+
+
+
+
+/* Stop Editing*/
+
+/* GET Request */
+$req;
+$val;
+
+/* Function Selector */
+if $req = track
+	track($val);
+if $req = view
+	view($val)
+
+
+public function track(){
+	# printrac.php?track
+	# printrac.php?track=debug
+	
+}
+
+	private function trackImg(){
+		# creates tracking images (only can be called for track function)
+	}
+
+public function view(){
+	# Dumps log file to screen with limited formatting
+}
+
+	private function gui(){
+		# Read log file and performs stats counts
+		# Parent to View
+	}
+
+
+public ga(){
+	/* Google Analytics Code
+	
+		Submit data to GA
+		
+	*/
+}
+
+/*Raw Data
+
+	Date/Time
+	IP
+	Host
+	User Agent
+	Current URL
+	Refer
+
+	written to txt file for log
+
+
 */
