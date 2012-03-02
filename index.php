0ddfa24df48c8480f04d1cc9ae3a7de205a08e48
Chris Holbrook <chris@madscitech.com>
2012-03-01 23:43:33 -0600

    Cleaned up page's design to fit with the rest of the application

--- index.php
+++ index.php
@@ -8,11 +8,12 @@
 	<![endif]-->
 </head>
 <body>
-	<a href="http://github.com/mad9scientist/PrinTrac.php/"><img style="position: fixed; top: 0; right: 0; border: 0; z-index: 10;" src="https://a248.e.akamai.net/assets.github.com/img/e6bef7a091f5f3138b8cd40bc3e114258dd68ddf/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub"></a>
+	<a href="http://github.com/mad9scientist/PrinTrac.php/"><img style="position: fixed; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/e6bef7a091f5f3138b8cd40bc3e114258dd68ddf/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub"></a>
 
 	<header>
 		<h1>PrinTrac.php</h1>
 		<span>How Often Are Your Webpages Being Printed?</span>
+		<a href="http://mad9scientist.com/" id="creator">M</a>
 	</header>
 
 	<article>
@@ -20,13 +21,15 @@
 	<h2>What is PrinTrac.php?</h2>
 	<p>PrinTrac.php is a simple script that tracks how often users print your web pages by using a invisible tracking image that is included in your print stylesheet.</p>
 
+	<a class="button" href="http://mad9scientist.com/projects/printracphp/">Try the Demo</a>
+
 	<h2>Concept and Origin</h2>
 	<p>This idea is from ShopTalkShow Host Chris Coyier and a user submitted question (Sorry I don't remember the listener's name) from <a href="http://shoptalkshow.com/episodes/006-with-zoe-gillenwater/" title="ShopTalk Show">ShopTalk Show - Episode 006 with Zoe Gillenwater</a>.</p>
 
 	<h2>Why Should I Use It?</h2>
 	<p>There isn't really a great reason to use it. It's just some simple statistics about your website visitor's printing habits.</p>
 
-	<h3>Installation</h3>
+	<h2>Installation and Use</h2>
 	<ol>
 		<li>Place printrac.php into your site's image directory or anywhere you want.</li>
 		<li>Create a directory in the same folder named 'logs'</li>
