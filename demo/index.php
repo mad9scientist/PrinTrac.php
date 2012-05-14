b178a12e4016fcea6511edbe2bb63ba919c8d82e
Chris Holbrook <chris@madscitech.com>
2012-05-14 01:30:28 -0500

    Updated GitHub Ribbon Image URL

--- demo/index.php
+++ demo/index.php
@@ -8,7 +8,7 @@
 	<![endif]-->
 </head>
 <body>
-	<a href="http://github.com/mad9scientist/PrinTrac.php/"><img alt="Fork me on GitHub" src="https://a248.e.akamai.net/assets.github.com/img/e6bef7a091f5f3138b8cd40bc3e114258dd68ddf/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" style="position: fixed; top: 0; right: 0; border: 0;"></a>
+	<a href="http://github.com/mad9scientist/PrinTrac.php/"><img alt="Fork me on GitHub" src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" style="position: fixed; top: 0; right: 0; border: 0;"></a>
 	<header>
 		<h1><a href="http://mad9scientist.com/projects/printracphp/" id="logo">PrinTrac.php</a></h1>
 		<span>Website Printing Stats, for the Curious</span>
